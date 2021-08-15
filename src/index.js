3a5821efbf041e6cf98f081450191c30ca6653c2
Uglekiska <uglekiska@yandex.by>
2021-08-15 16:45:40 +0300

    fix: end index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    let abs = Math.abs(n);
+    return abs = (typeof abs === 'number') ? abs.toString().split('').reverse().join('') : 'Not a number';
 }
